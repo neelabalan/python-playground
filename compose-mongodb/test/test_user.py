0dd24e94f3143edbdaec4cec41f93a35c3881081
neelabalan <41639065+neelabalan@users.noreply.github.com>
2021-03-02 12:30:14 +0530

    boilerplate

--- compose-mongodb/test/test_user.py
+++ compose-mongodb/test/test_user.py
@@ -1,10 +1,4 @@
 import json
-import unittest
-
-from models import User
-from models.abc import db
-from repositories import UserRepository
-from server import server
 
 # user pytest
 class TestUser(unittest.TestCase):
